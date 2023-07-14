f40a18b2099e5c3516345fbdb4f497a8575b7ce9
ak9391 <ak9391@nyu.edu>
2023-07-14 20:09:19 +0530

    Initial Commit

---
+++ GitHub Pair/GitHub_PairApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PairApp.swift
+//  GitHub Pair
+//
+//  Created by Ashrita Kantamneni on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
